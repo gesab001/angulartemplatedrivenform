7e59ef376262227afe988cdbe022b20380c10648
gesab001 <144000giovanni@gmail.com>
2019-03-15 12:11:05 +1300

    first commit

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('Welcome to new!');
+    expect(page.getTitleText()).toEqual('Welcome to angularcli!');
   });
 
   afterEach(async () => {
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -19,13 +19,13 @@ describe('AppComponent', () => {
   it(`should have as title 'new'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('new');
+    expect(app.title).toEqual('angularcli');
   });
 
   it('should render title in a h1 tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to new!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to angularcli!');
   });
 });
